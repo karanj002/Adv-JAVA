1740f94a0967c8b8a34f973f763daf3239d2bbd7
Karan Jadhav <75842930+karanj002@users.noreply.github.com>
2023-11-19 18:53:57 +0530

    Add files via upload
    Abstract class : A class which is declared with the abstract keyword is known as an abstract class in Java. It can have abstract and non-abstract methods (method with the body).
    Abstraction is a process of hiding the implementation details and showing only functionality to the user.

---
+++ FullTime.java
@@ -0,0 +1,9 @@
+package Abstract_class;
+
+public class FullTime extends Employee{
+
+    @Override
+    public String getStatus(){
+        return "Full time";
+    }
+}
---
+++ PartTime.java
@@ -0,0 +1,8 @@
+package Abstract_class;
+
+public class PartTime extends Employee {
+    @Override
+    public String getStatus(){
+        return "Part Time";
+    }
+}
